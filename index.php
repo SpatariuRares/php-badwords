23a1796cf33b8f710d51259ece7af9b8196ca633
spata00 <sararares12@gmail.com>
2021-10-07 13:31:08 +0200

    finito

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-    $name="rares";
+    $badWord="santo";
+    $paragraph="marco giovanni filippo gatto cane ";
 ?>
-<h1><?php echo $name; ?></h1>
+<h1>
+    <form method="get">
+        <input type="text" name="badWord" placeholder="ciao">
+        <input type="submit" value="ciao">
+    </form>
+    <?php 
+        echo str_replace($_GET["badWord"],"***",$paragraph);
+        echo strlen($paragraph);
+    ?>
+</h1>
